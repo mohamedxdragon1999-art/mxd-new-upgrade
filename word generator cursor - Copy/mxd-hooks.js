8dd14340aa9418d054c1ebfdb57d13c0e46b1c5c
mohamedxdragon1999-art <mohamedxdragon1999@gmail.com>
2026-05-16 06:43:10 +0300

    Delete word generator cursor - Copy directory

--- word generator cursor - Copy/mxd-hooks.js
+++
@@ -1,8 +0,0 @@
-﻿// MXD React Hooks - Use __MXD_HOOKS__ for components
-window.MXDReactHooks = window.__MXD_HOOKS__ || {
-    useState: window.React?.useState,
-    useEffect: window.React?.useEffect,
-    useCallback: window.React?.useCallback,
-    useRef: window.React?.useRef,
-    useMemo: window.React?.useMemo
-};
